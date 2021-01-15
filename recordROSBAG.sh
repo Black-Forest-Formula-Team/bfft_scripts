e10f5eb409c0fb414ed1542c819de5f7fa16f1ef
Your Name <bf-formula@hs-offenburg.de>
2021-01-15 17:15:32 +0100

    AD0021 - Integration Testing SSH conn Ubuntu-Win, adapted bash scripts to launch from Windows

--- recordROSBAG.sh
+++ recordROSBAG.sh
@@ -5,5 +5,5 @@
 echo 'Starting Recording of CSV File. Parameters need to be adjusted in launch file.'
 
 cd /SSD_512/bagfiles && rosbag record -a 
-
+#--pid=/tmp/rosbag_process.pid 
 echo "Bash Script ROS END"
--- startROS.sh
+++ startROS.sh
@@ -4,6 +4,10 @@
 
 echo 'Starting ROS Core and launch file for Data collection!'
 
-roslaunch convert_can_data_to_topic Start_Data_Collection.launch 
+roslaunch --pid=/tmp/myroslaunch.pid convert_can_data_to_topic Start_Data_Collection.launch &
+P1=$!
+cd /SSD_512/bagfiles && rosbag record -a &
+P2=$!
+wait $P1 $P2
 
 echo "Bash Script ROS END"
