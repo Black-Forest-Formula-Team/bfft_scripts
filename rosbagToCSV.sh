b619fbf98eefad7993b4a7d9d82d9d965b1d1eb2
Your Name <bf-formula@hs-offenburg.de>
2021-03-16 17:57:21 +0100

    AD0033 - "Update paths to ROS launch files"

--- rosbagToCSV.sh
+++ rosbagToCSV.sh
@@ -4,7 +4,7 @@
 
 echo 'Starting launch file for conversion of ROSBAG to CSV files (one per topic). Parameters need to be adjusted in launch file.'
 
-roslaunch data_to_storage ROSBAG_to_CSV.launch
+roslaunch bfft_data_conversion ROSBAG_to_CSV.launch
 #wait $P1 $P2
 
 echo "Bash Script ROS END"
