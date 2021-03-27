63b1a4de101d6e076b443fe43e69c4fab5059c8e
Your Name <bf-formula@hs-offenburg.de>
2021-03-27 19:07:26 +0100

    AD0024 - PowerShell Automation (remote control via Windows)

--- stopROS.sh
+++ stopROS.sh
@@ -6,6 +6,8 @@ rosnode list | grep record* | xargs rosnode kill
 
 kill -INT `cat /tmp/myroslaunch.pid` 
 
+roslaunch bfft_data_conversion ROSBAG_to_CSV.launch
+
 echo "Bash Script ROS END"
 
 ## http://ros-users.122217.n3.nabble.com/Kill-all-running-ros-processes-td763356.html
